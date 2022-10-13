4eb886d8f11d21814a65be0b31b325ba4211eb09
ankitppatel07 <ankitppatel07@gmail.com>
2022-10-13 00:11:08 -0400

    app structure

--- univ_portal-client/src/app/app.module.ts
+++ univ_portal-client/src/app/app.module.ts
@@ -6,6 +6,13 @@ import { HomeComponent } from './components/home/home.component';
 
 import { RouterModule, Routes } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
+import { AdminComponent } from './admin/admin.component';
+import { LoginComponent } from './admin/login/login.component';
+import { RegisterComponent } from './admin/register/register.component';
+import { UserListComponent } from './admin/user-list/user-list.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { NavComponent } from './components/shared/nav/nav.component';
 
 
 const routes: Routes = [
@@ -17,7 +24,14 @@ const routes: Routes = [
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    AdminComponent,
+    LoginComponent,
+    RegisterComponent,
+    UserListComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
